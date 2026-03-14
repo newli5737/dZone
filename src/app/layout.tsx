5e26797168aa9ee0bc00eaba33c8296ec47b06b6
laingoc99 <laingoc99@gmail.com>
2026-03-14 12:20:31 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,10 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "dlBiz - Khám phá Buôn Ma Thuột",
   description: "Tổng hợp toplist nhà hàng, quán ăn, dịch vụ tại Buôn Ma Thuột được dlBiz bình chọn và chia sẻ.",
+  icons: {
+    icon: "/images/logo.webp",
+    apple: "/images/logo.webp",
+  },
 };
 
 export default function RootLayout({
